5fe349520faf7c5ee1ea5e9e423d683c900bc917
ephtron <es.90@gmx.de>
2016-06-02 03:10:48 +0200

    added listview, label and closebutton to the preset_widget

--- qt/DsaMediaControlKit/ui/preset_widget.cpp
+++ qt/DsaMediaControlKit/ui/preset_widget.cpp
@@ -1,17 +1,65 @@
 #include "preset_widget.h"
 
+#include <QHBoxLayout>
+#include <QDebug>
 
 namespace UI {
 
-PresetWidget::PresetWidget(QWidget *parent)
+PresetWidget::PresetWidget(QWidget *parent, int id, QString name)
     : QWidget(parent)
+    , id_(id)
     , preset_()
+    , label_(0)
+    , list_view_(0)
 {
-
+    preset_ = new Preset::Preset(this, name);
+    initWidgets();
+    initLayout();
 }
 
 PresetWidget::~PresetWidget()
 {
+
+}
+
+int PresetWidget::getID() const
+{
+    return id_;
+}
+
+void PresetWidget::onClosedClicked(bool)
+{
+    qDebug() << "closed preset widget " << preset_->name();
+    emit closed(id_);
+}
+
+void PresetWidget::initWidgets()
+{
+    QList<DB::SoundFileRecord*> temp;
+    list_view_ = new UI::SoundFileListView(temp,this);
+
+    label_ = new QLabel(preset_->name(),this);
+    close_button_ = new QPushButton("x", this);
+
+    connect(close_button_, SIGNAL(clicked(bool)),
+            this, SLOT(onClosedClicked(bool)));
+}
+
+void PresetWidget::initLayout()
+{
+    QVBoxLayout* layout = new QVBoxLayout;
+
+    QHBoxLayout* upper = new QHBoxLayout;
+
+    QHBoxLayout* list_view_layout = new QHBoxLayout;
+    list_view_layout->addWidget(list_view_);
+
+    upper->addWidget(label_);
+    upper->addWidget(close_button_);
+    layout->addLayout(upper);
+    layout->addLayout(list_view_layout);
+
+    setLayout(layout);
 
 }
 
--- qt/DsaMediaControlKit/ui/preset_widget.h
+++ qt/DsaMediaControlKit/ui/preset_widget.h
@@ -2,7 +2,12 @@
 #define PRESETWIDGET_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QGroupBox>
+
 #include "presets/preset.h"
+#include "ui/sound_file_list_view.h"
 
 namespace UI {
 
@@ -10,19 +15,32 @@ class PresetWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit PresetWidget(QWidget *parent = 0);
+    explicit PresetWidget(QWidget *parent = 0, int id = -1, QString name = "Preset");
     ~PresetWidget();
 
-public slots:
 
+    int getID() const;
 
 
 signals:
+    void closed(int);
+
+
+public slots:
+    void onClosedClicked(bool);
 
 public slots:
 
 private:
+    void initWidgets();
+    void initLayout();
+
+
+    int id_;
     Preset::Preset* preset_;
+    QPushButton* close_button_;
+    QLabel* label_;
+    UI::SoundFileListView* list_view_;
 
 };
 
