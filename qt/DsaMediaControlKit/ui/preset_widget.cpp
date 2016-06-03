4fa75f6c884e13d9769bfe9d897093c58dc586c7
ephtron <es.90@gmx.de>
2016-06-03 03:12:21 +0200

    preset widget class holds playlist views of a preset

--- qt/DsaMediaControlKit/ui/preset_widget.cpp
+++ qt/DsaMediaControlKit/ui/preset_widget.cpp
@@ -5,14 +5,49 @@
 
 namespace UI {
 
-PresetWidget::PresetWidget(QWidget *parent, int id, QString name)
+PresetWidget::PresetWidget(QString name, QWidget *parent, int id)
     : QWidget(parent)
     , id_(id)
+    , playlist_id_iterator_(0)
     , preset_()
+    , add_playlist_button_(0)
+    , close_button_(0)
     , label_(0)
-    , list_view_(0)
+    , playlists_widgets_()
+    , widget_layout_(0)
 {
-    preset_ = new Preset::Preset(this, name);
+    preset_ = new Preset::Preset(name, this);
+    initWidgets();
+    initLayout();
+}
+
+PresetWidget::PresetWidget(Preset::Preset *preset, QWidget *parent, int id)
+    : QWidget(parent)
+    , id_(id)
+    , playlist_id_iterator_(0)
+    , preset_(preset)
+    , add_playlist_button_(0)
+    , close_button_(0)
+    , label_(0)
+    , playlists_widgets_()
+    , widget_layout_(0)
+{
+    initWidgets();
+    initLayout();
+}
+
+PresetWidget::PresetWidget(DB::SoundFileRecord *sound_file, QWidget *parent, int id)
+    : QWidget(parent)
+    , id_(id)
+    , playlist_id_iterator_(0)
+    , preset_()
+    , add_playlist_button_(0)
+    , close_button_(0)
+    , label_(0)
+    , playlists_widgets_()
+    , widget_layout_(0)
+{
+    preset_ = new Preset::Preset(sound_file->name, sound_file, this);
     initWidgets();
     initLayout();
 }
@@ -27,39 +62,78 @@ int PresetWidget::getID() const
     return id_;
 }
 
+void PresetWidget::addPlaylist()
+{
+    addPresetPlaylist(playlist_id_iterator_);
+    ++playlist_id_iterator_;
+    qDebug() << "Debug Preset Widget: adding playlist " << playlist_id_iterator_;
+}
+
+void PresetWidget::removePlaylist(int id)
+{
+    removePresetPlaylist(id);
+}
+
 void PresetWidget::onClosedClicked(bool)
 {
-    qDebug() << "closed preset widget " << preset_->name();
+    qDebug() << "closed preset widget " << preset_->getName();
     emit closed(id_);
+}
+
+void PresetWidget::addPresetPlaylist(int id)
+{
+    if(playlists_widgets_.contains(id)) {
+        qDebug() << "PresetWidget note: Playlist with ID" << id << "already exists.";
+        return;
+    }
+    QString name = "Playlist"+QString::number(id);
+    PresetPlaylist* playlist = new PresetPlaylist(name, this, id);
+    playlists_widgets_.insert(id, playlist);
+
+    widget_layout_->addWidget(playlists_widgets_[id]);
+
+    connect(playlists_widgets_[id], SIGNAL(closed(int)),
+            this, SLOT(removePlaylist(int)));
+}
+
+void PresetWidget::removePresetPlaylist(int id)
+{
+    if(!playlists_widgets_.contains(id))
+        return;
+
+    widget_layout_->removeWidget(playlists_widgets_[id]);
+    delete playlists_widgets_[id];
+    playlists_widgets_.remove(id);
 }
 
 void PresetWidget::initWidgets()
 {
     QList<DB::SoundFileRecord*> temp;
-    list_view_ = new UI::SoundFileListView(temp,this);
 
-    label_ = new QLabel(preset_->name(),this);
+    label_ = new QLabel(preset_->getName(),this);
+    add_playlist_button_ = new QPushButton("new Playlist", this);
     close_button_ = new QPushButton("x", this);
 
+    connect(add_playlist_button_, SIGNAL(clicked(bool)),
+            this, SLOT(addPlaylist()));
     connect(close_button_, SIGNAL(clicked(bool)),
             this, SLOT(onClosedClicked(bool)));
+
 }
 
 void PresetWidget::initLayout()
 {
-    QVBoxLayout* layout = new QVBoxLayout;
+    widget_layout_ = new QVBoxLayout;
 
     QHBoxLayout* upper = new QHBoxLayout;
 
-    QHBoxLayout* list_view_layout = new QHBoxLayout;
-    list_view_layout->addWidget(list_view_);
 
     upper->addWidget(label_);
+    upper->addWidget(add_playlist_button_);
     upper->addWidget(close_button_);
-    layout->addLayout(upper);
-    layout->addLayout(list_view_layout);
+    widget_layout_->addLayout(upper);
 
-    setLayout(layout);
+    setLayout(widget_layout_);
 
 }
 
--- qt/DsaMediaControlKit/ui/preset_widget.h
+++ qt/DsaMediaControlKit/ui/preset_widget.h
@@ -4,10 +4,11 @@
 #include <QWidget>
 #include <QLabel>
 #include <QPushButton>
-#include <QGroupBox>
+#include <QHBoxLayout>
 
 #include "presets/preset.h"
 #include "ui/sound_file_list_view.h"
+#include "ui/preset_playlist.h"
 
 namespace UI {
 
@@ -15,12 +16,12 @@ class PresetWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit PresetWidget(QWidget *parent = 0, int id = -1, QString name = "Preset");
+    explicit PresetWidget(QString name ,QWidget *parent = 0, int id = -1);
+    explicit PresetWidget(Preset::Preset *preset, QWidget *parent = 0, int id = -1);
+    explicit PresetWidget(DB::SoundFileRecord *sound_file, QWidget *parent = 0, int id = -1);
     ~PresetWidget();
 
-
     int getID() const;
-
 
 signals:
     void closed(int);
@@ -28,19 +29,27 @@ signals:
 
 public slots:
     void onClosedClicked(bool);
+    void addPlaylist();
+    void removePlaylist(int id);
 
 public slots:
 
 private:
+    void addPresetPlaylist(int id);
+    void removePresetPlaylist(int id);
+
     void initWidgets();
     void initLayout();
 
 
     int id_;
+    int playlist_id_iterator_;
     Preset::Preset* preset_;
+    QPushButton* add_playlist_button_;
     QPushButton* close_button_;
     QLabel* label_;
-    UI::SoundFileListView* list_view_;
+    QMap<int, PresetPlaylist*> playlists_widgets_;
+    QVBoxLayout* widget_layout_;
 
 };
 
