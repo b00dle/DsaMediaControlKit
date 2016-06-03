ba1dbea1eb8650ff79134d0144afb614aab9c002
ephtron <es.90@gmx.de>
2016-06-03 03:11:21 +0200

    added playlistwidget with a drag and drop list view

--- qt/DsaMediaControlKit/ui/preset_playlist.h
+++ qt/DsaMediaControlKit/ui/preset_playlist.h
@@ -3,15 +3,39 @@
 
 #include <QWidget>
 
+#include <QLineEdit>
+#include <QPushButton>
+#include <QGroupBox>
+#include "presets/playlist.h"
+#include "ui/sound_file_list_view.h"
+namespace UI{
+
 class PresetPlaylist : public QWidget
 {
     Q_OBJECT
 public:
-    explicit PresetPlaylist(QWidget *parent = 0);
+    explicit PresetPlaylist(QString name = "playlist", QWidget *parent = 0, int id = -1);
+    explicit PresetPlaylist(Preset::Playlist* playlist, QWidget *parent = 0, int id = -1);
+    ~PresetPlaylist();
 
 signals:
+    void closed(int);
 
 public slots:
+    void onClosedClicked(bool);
+
+private:
+    void initWidgets();
+    void initLayout();
+
+    int id_;
+    Preset::Playlist* playlist_;
+    QPushButton* close_button_;
+    QLineEdit* label_;
+    UI::SoundFileListView* list_view_;
+
 };
+
+} //namespace ui
 
 #endif // PRESETPLAYLIST_H
