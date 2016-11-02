848e0ea5b0bdac96710786db4bedb2dc49f6bd5d
ephtron <es.90@gmx.de>
2016-11-02 16:12:52 +0100

    renamed and modified playlist settings struct, now has min, max interval values and a interval flag, added new constructors

--- qt/DsaMediaControlKit/playlist/settings.h
+++ qt/DsaMediaControlKit/playlist/settings.h
@@ -1,6 +1,8 @@
 #ifndef SETTINGS
 #define SETTINGS
 
+#include <QString>
+
 namespace Playlist{
 
 /*
@@ -18,19 +20,46 @@ enum PlayOrder{
 /*
  * Struct describing how a playlist should play its sound files.
 */
-struct PlaylistSettings{
+struct Settings{
 
+
+    QString name;
     PlayOrder order;
-    bool repeat;
-    QString interval;
+    bool loop_flag;
+    bool interval_flag;
+    int min_delay_interval;
+    int max_delay_interval;
     int volume;
 
 
-    PlaylistSettings()
-        :order(ORDERED)
-        ,repeat(true)
-        ,interval()
-        ,volume(-1)
+    Settings()
+        : name("Settings")
+        , order(PlayOrder::ORDERED)
+        , loop_flag(true)
+        , interval_flag(false)
+        , min_delay_interval(0)
+        , max_delay_interval(0)
+        , volume(-1)
+    {}
+
+    Settings(QString n,PlayOrder ord, bool loop, bool interval, int min_interval, int max_interval, int vol)
+        : name(n)
+        , order(ord)
+        , loop_flag(loop)
+        , interval_flag(interval)
+        , min_delay_interval(min_interval)
+        , max_delay_interval(max_interval)
+        , volume(vol)
+    {}
+
+    Settings(Settings *settings)
+        : name(settings->name)
+        , order(settings->order)
+        , loop_flag(settings->loop_flag)
+        , interval_flag(settings->interval_flag)
+        , min_delay_interval(settings->min_delay_interval)
+        , max_delay_interval(settings->max_delay_interval)
+        , volume(settings->volume)
     {}
 };
 
